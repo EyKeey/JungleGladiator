e145b81bc8d821f5faaf291e200cbbef21b36d6f
EyKeey <alprnkkc@gmail.com>
2024-08-02 23:28:25 +0300

    Upgrade system added.

---
+++ scenes/UI/ability_upgrade_card.gd
@@ -0,0 +1,8 @@
+extends Panel
+
+@onready var name_label: Label = $VBoxContainer/NameLabel
+@onready var description_label: Label = $VBoxContainer/DescriptionLabel
+
+func set_ability_upgrade(upgrade: AbilityUpgrade):
+	name_label.text = upgrade.name
+	description_label.text = upgrade.description
